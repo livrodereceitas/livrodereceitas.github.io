f2263e8a74e5af528b26714ed502574bc87546e2
Sabrina Torres <sabrinaatorres97@gmail.com>
2025-09-19 21:57:38 -0300

    prototype: nav below header

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# livrodereceitas.github.io
 Website do Livro de Receitas :3
+
+Abra uma PR ou entre em contato (sabsazaria at proton.me) caso tenha alguma contribuição.
--- ldr/src/App.js
+++ ldr/src/App.js
@@ -3,13 +3,16 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="Header">
         <h1 className='Title'>
           Livro de Receitas
         </h1>
         <h4 className='Title'>
           Um guia teórico e prático para segurança digital, focada em ativismo político e juventude LGBT+.
         </h4>
+        <div class="ButtonGroup">
+          <button>Quem somos</button>
+        </div>
       </header>
     </div>
   );
