0e0d462e8db3d55168e9a8d42e016ae900f94de7
Sabrina Torres <sabrinaatorres97@gmail.com>
2025-09-19 21:37:13 -0300

    prototype: starting layout

---
+++ ldr/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className='Title'>
+          Livro de Receitas
+        </h1>
+        <h4 className='Title'>
+          Um guia teórico e prático para segurança digital, focada em ativismo político e juventude LGBT+.
+        </h4>
+      </header>
+    </div>
+  );
+}
+
+export default App;
